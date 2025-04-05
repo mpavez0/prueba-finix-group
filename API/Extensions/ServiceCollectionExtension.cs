9bacc3052cb7649d2ac54ca08c4e467f2339b7f4
Marco Pavez <marcopz03@outlook.com>
2025-04-04 23:57:14 -0300

    Modifica inicialización de servicios

---
+++ API/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,24 @@
+﻿using GestorFacturas.API.Middlewares;
+using GestorFacturas.Infrastructure.Data;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorFacturas.Domain.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection ServiceGeneric(this IServiceCollection services)
+        {
+            services.AddTransient<IDataSeeder, DataSeeder>();
+            return services;
+        }
+        public static IApplicationBuilder UseAuthorizationMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AuthorizationMiddleware>();
+        }
+    }
+}

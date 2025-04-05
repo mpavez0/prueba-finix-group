75ef581d3ed66e67a7b47c63c14a98710b479dac
Marco Pavez <marcopz03@outlook.com>
2025-04-04 23:53:08 -0300

    Corrige migración de base de datos

--- Infrastructure/Migrations/20250405024242_InvoiceDBMigration.Designer.cs
+++ Infrastructure/Migrations/20250405024242_InvoiceDBMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GestorFacturas.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GestorFacturas.Infrastructure.Migrations
 {
     [DbContext(typeof(InvoiceDbContext))]
-    partial class InvoiceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250405024242_InvoiceDBMigration")]
+    partial class InvoiceDBMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.14");
